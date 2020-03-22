d3fb39a0865da659e0019ef36503033b7b8c38a6
Hepic <hepic135@gmail.com>
2020-03-22 19:28:36 +0200

    updates

--- bonus.cpp
+++ bonus.cpp
@@ -24,6 +24,26 @@ typedef Cartesian <NT> Kernel;
 typedef typename Kernel::Point Point;
 typedef HPolytope <Point> Hpolytope;
 
+// auxilary variables 
+int walk_len = 10, nsam = 1000;
+int n, N = 20, W = 20, n_threads = 1;
+int rnum = pow(0.1,-2) * 400 * n * log(n);
+double a = 0.5;
+
+bool user_W=false, user_N=false, user__ratio=false, user_NN = false, set_algo = false, set_error = false;
+NT ball_radius=0.0, diameter = -1.0, lb = 0.1, ub = 0.15, p = 0.75, rmax = 0.0, alpha = 0.2, _round_val = 1.0;
+NT C=2.0,_ratio,frac=0.1,delta=-1.0,error=0.1;
+bool birk=false, rotate=false, _ball_walk=false, ball_rad=false, experiments=true, CG = false, Vpoly=false, Zono=false, cdhr=false, 
+rdhr=false,user_randwalk = false, exact_zono = false, gaussian_sam = false, hpoly = false, billiard=false, win2 = false,
+boundary = false, verbose=false, rand_only=false, _round_only=false, file=false, _round=false, NN=false, user_walk_len=false, 
+linear_extensions=false;
+
+unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+RNGType rng(seed);
+boost::normal_distribution<> rdist(0,1);
+boost::random::uniform_real_distribution<>(urdist);
+boost::random::uniform_real_distribution<> urdist1(-1, 1);
+
 
 double func(Point p1, Point p2, double a) {
     int dim = p1.dimension();
@@ -38,33 +58,13 @@ double func(Point p1, Point p2, double a) {
     return exp(-a * sum);
 }
 
-double monteCarlo(Hpolytope HP, pair<Point, NT> InnerBall) {
-    // auxilary variables 
-    int walk_len = 10, nsam = 1000;
-    int n = HP.dimension(), N = 20, W = 20, n_threads = 1;
-    int rnum = pow(0.1,-2) * 400 * n * log(n);
-    double a = 0.5;
-
-    bool user_W=false, user_N=false, user_ratio=false, user_NN = false, set_algo = false, set_error = false;
-    NT ball_radius=0.0, diameter = -1.0, lb = 0.1, ub = 0.15, p = 0.75, rmax = 0.0, alpha = 0.2, round_val = 1.0;
-    NT C=2.0,ratio,frac=0.1,delta=-1.0,error=0.1;
-    bool birk=false, rotate=false, ball_walk=false, ball_rad=false, experiments=true, CG = false, Vpoly=false, Zono=false, cdhr=false, 
-    rdhr=false,user_randwalk = false, exact_zono = false, gaussian_sam = false, hpoly = false, billiard=false, win2 = false,
-    boundary = false, verbose=false, rand_only=false, round_only=false, file=false, round=false, NN=false, user_walk_len=false, 
-    linear_extensions=false;
-
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    RNGType rng(seed);
-    boost::normal_distribution<> rdist(0,1);
-    boost::random::uniform_real_distribution<>(urdist);
-    boost::random::uniform_real_distribution<> urdist1(-1, 1);
-
+double monteCarlo(Hpolytope HP, pair<Point, NT> InnerBall, double vol) {
     double radius = InnerBall.second;
     
     vars<NT, RNGType> var1(0, n, walk_len, 1, 0, 0, 0, 0.0, 0, radius, diameter, rng,
-            urdist, urdist1, delta, verbose, rand_only, round, NN, birk, ball_walk, cdhr, rdhr,billiard);
-    vars_g<NT, RNGType> var2(n, walk_len, N, W, 1, 0, radius, rng, C, frac, ratio, delta,
-          verbose, rand_only, round, NN, birk, ball_walk, cdhr, rdhr);
+            urdist, urdist1, delta, verbose, rand_only, _round, NN, birk, _ball_walk, cdhr, rdhr,billiard);
+    vars_g<NT, RNGType> var2(n, walk_len, N, W, 1, 0, radius, rng, C, frac, _ratio, delta,
+          verbose, rand_only, _round, NN, birk, _ball_walk, cdhr, rdhr);
 
     // Monte Carlo implementation
     Point x0 = InnerBall.first;
@@ -77,13 +77,7 @@ double monteCarlo(Hpolytope HP, pair<Point, NT> InnerBall) {
     for (auto pnt: randPoints) {
         sum += func(pnt, x0, 0.5);
     }
-    
-    /*vars<NT, RNGType> var(rnum, n, walk_len, n_threads, error, 0.1,0,0.0,0, radius,diameter,rng,
-               urdist,urdist1,delta,verbose,rand_only,round,NN,birk,ball_walk,cdhr,rdhr,billiard);
-
-    double vol = volume(HP, var, InnerBall);*/
-    double vol = 1;
-    
+      
     return sum * vol / (double)nsam;
 }
 
@@ -93,12 +87,22 @@ int main(const int argc, const char** argv) {
     Hpolytope HP = random_hpoly<Hpolytope, RNGType>(10, 500);
     pair<Point, NT> InnerBall = HP.ComputeInnerBall();
     
+    n = HP.dimension();
+
+    vars_g <NT, RNGType> _var1(n, walk_len, N, W, 1, error, InnerBall.second, rng, C, frac, _ratio, delta,
+                              verbose, rand_only, _round, NN, birk, _ball_walk, cdhr, rdhr);
+    vars <NT, RNGType> _var2(rnum, n, 10 + n / 10, n_threads, error, error, 0, 0.0, 0, InnerBall.second, diameter, rng,
+                             urdist, urdist1, delta, verbose, rand_only, _round, NN, birk, _ball_walk, cdhr,
+                             rdhr,billiard);
+
+    double vol = volume_gaussian_annealing(HP, _var1, _var2, InnerBall);
+    
     int repet = 20;
     double mean = 0;
     vector<double> values;
     
     for (int t = 1; t <= repet; ++t) {
-        values.push_back(monteCarlo(HP, InnerBall));
+        values.push_back(monteCarlo(HP, InnerBall, vol));
         mean += values.back();
     }
     
@@ -112,6 +116,6 @@ int main(const int argc, const char** argv) {
     sum /= (repet - 1);
     double std = sqrt(sum);
 
-    cout << "integral = " << mean << " standard deviation = " << std << endl;
+    cout << "integral = " << mean << "  standard deviation = " << std << endl;
     return 0;
 }
